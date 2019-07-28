73d6f8f3a63e2a8e5089f7a6b76e48f26ececd58
Robert Kanyur <robert@kanyur.com>
2019-07-27 21:54:00 -0700

    Basic sendgrid implementation

--- emailer.py
+++ emailer.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, abort
 from dotenv import load_dotenv
 import os
 from sendgrid import SendGridAPIClient
@@ -15,16 +15,26 @@ def status_page():
 def send_email():
     if request.method == 'GET':
         return render_template('main.html', content='<strong>Access denied.</strong><br /><br />Your IP address has been logged and this incident has been reported to the authorities.')
+    if not request.json or not 'messageSource' in request.json or not 'fromEmail' in request.json or not 'toEmail' in request.json or not 'emailSubject' in request.json or not 'emailHTML' in request.json:
+        abort(400);
+    if not request.headers.get('Authorization') == ('Bearer ' + os.getenv('AUTH')):
+        abort(403);
+    message_source = request.json['messageSource']
     message = Mail(
-        from_email='robert@theoakco.com',
-        to_emails='robertkanyur+a@gmail.com',
-        subject='test message 2',
-        html_content='test successful',
+        from_email=request.json['fromEmail'],
+        to_emails=request.json['toEmail'],
+        subject=request.json['emailSubject'],
+        html_content=request.json['emailHTML'],
     )
-
     try:
-        sg = SendGridAPIClient(os.getenv('SENDGRID'))
-        response = sg.send(message)
-        return render_template('main.html', content=response.body)
+        if os.getenv('SENDGRID_ENABLED') == 'True':
+            sg = SendGridAPIClient(os.getenv('SENDGRID'))
+            response = sg.send(message)
+            return(str(response.status_code))
+        else:
+            return('200')
     except Exception as e:
-        return(str(e))
+        if os.getenv('SENDGRID_ENABLED') == 'True':
+            return(str(e))
+        else:
+            abort(500)
