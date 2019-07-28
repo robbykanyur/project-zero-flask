730abec0dddc33970cb5601c8494fc9bcbabddbe
Robert Kanyur <robert@kanyur.com>
2019-07-27 21:09:23 -0700

    Basic templates

--- emailer.py
+++ emailer.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from dotenv import load_dotenv
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 app = Flask(__name__)
 load_dotenv()
 
 @app.route('/')
-def hello_world():
-    return 'hello world'
+def status_page():
+    return render_template('main.html', content='The <strong>emailer</strong> microservice is running.')
+
+@app.route('/api/v1', methods=['GET','POST'])
+def send_email():
+    if request.method == 'GET':
+        return render_template('main.html', content='<strong>Access denied.</strong><br /><br />Your IP address has been logged and this incident has been reported to the authorities.')
+    message = Mail(
+        from_email='robert@theoakco.com',
+        to_emails='robertkanyur+a@gmail.com',
+        subject='test message 2',
+        html_content='test successful',
+    )
+
+    try:
+        sg = SendGridAPIClient(os.getenv('SENDGRID'))
+        response = sg.send(message)
+        return render_template('main.html', content=response.body)
+    except Exception as e:
+        return(str(e))
